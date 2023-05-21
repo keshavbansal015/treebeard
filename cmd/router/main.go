feb9ebefbb86e73b8a326e6e733a11067174bf58
aminst <aminst79@gmail.com>
2023-05-22 00:26:51 +0330

    Refactor router to read shardnode endpoints from yaml file

--- cmd/router/main.go
+++ cmd/router/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"log"
 
+	"github.com/dsg-uwaterloo/oblishard/pkg/config"
 	router "github.com/dsg-uwaterloo/oblishard/pkg/router"
 )
 
 func main() {
-	shardNodeRPCClients, err := router.StartRPCClients()
+	shardNodeEndpoints, err := config.ReadEndpoints("../../configs/router_endpoints.yaml")
+	if err != nil {
+		log.Fatalf("Cannot read shard node endpoints from yaml file; %v", err)
+	}
+	shardNodeRPCClients, err := router.StartRPCClients(shardNodeEndpoints)
 	if err != nil {
 		log.Fatalf("Failed to create client connections with shard node servers; %v", err)
 	}
--- pkg/router/client.go
+++ pkg/router/client.go
@@ -4,32 +4,27 @@ import (
 	"fmt"
 
 	shardnodepb "github.com/dsg-uwaterloo/oblishard/api/shardnode"
+	"github.com/dsg-uwaterloo/oblishard/pkg/config"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
-
-var shardNodeIdToPort = map[int]int{
-	0: 8766,
-	1: 8767,
-	2: 8768,
-}
 
 type RPCClient struct {
 	ClientAPI shardnodepb.ShardNodeClient
 	Conn      *grpc.ClientConn
 }
 
-func StartRPCClients() (clients map[int]RPCClient, err error) {
+func StartRPCClients(shardNodeEndpoints []config.Endpoint) (clients map[int]RPCClient, err error) {
 	clients = make(map[int]RPCClient)
-	for id, port := range shardNodeIdToPort {
-		serverAddr := fmt.Sprintf("localhost:%d", port)
+	for _, shardNodeEndpoint := range shardNodeEndpoints {
+		serverAddr := fmt.Sprintf("%s:%d", shardNodeEndpoint.IP, shardNodeEndpoint.Port)
 		conn, err := grpc.Dial(serverAddr,
 			grpc.WithTransportCredentials(insecure.NewCredentials())) //TODO change to use TLS if needed
 		if err != nil {
 			return nil, err
 		}
 		clientAPI := shardnodepb.NewShardNodeClient(conn)
-		clients[id] = RPCClient{ClientAPI: clientAPI, Conn: conn}
+		clients[shardNodeEndpoint.ID] = RPCClient{ClientAPI: clientAPI, Conn: conn}
 	}
 	return clients, nil
 }
