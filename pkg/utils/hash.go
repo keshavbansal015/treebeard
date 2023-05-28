4b1f647a74cc8ed3fb8d1fe57830af41221c240f
aminst <aminst79@gmail.com>
2023-05-28 15:09:29 +0330

    Add TODO comment to later optimize the hashing logic

--- pkg/utils/hash.go
+++ pkg/utils/hash.go
@@ -2,7 +2,7 @@ package utils
 
 import "hash/fnv"
 
-func Hash(s string) uint32 {
+func Hash(s string) uint32 { //TODO: optimize for faster hashing
 	h := fnv.New32a()
 	h.Write([]byte(s))
 	return h.Sum32()
