0569118e2e3383b5ac52ddb4981b3a76cb0c4c00
aminst <aminst79@gmail.com>
2023-05-30 13:42:01 +0330

    Add script for generating proto files

---
+++ scripts/generate_protos.sh
@@ -0,0 +1,7 @@
+PROTOBUF_PATH=../api
+
+mkdir -p $PROTOBUF_PATH/router
+protoc --proto_path=$PROTOBUF_PATH --go_out=$PROTOBUF_PATH/router --go_opt=paths=source_relative --go-grpc_out=$PROTOBUF_PATH/router --go-grpc_opt=paths=source_relative router.proto
+
+mkdir -p $PROTOBUF_PATH/shardnode
+protoc --proto_path=$PROTOBUF_PATH --go_out=$PROTOBUF_PATH/shardnode --go_opt=paths=source_relative --go-grpc_out=$PROTOBUF_PATH/shardnode --go-grpc_opt=paths=source_relative shardnode.proto
