86c1fb8578c5b4da685c8a10eb9661465c11b1c0
aminst <aminst79@gmail.com>
2023-06-06 12:13:36 +0330

    Fix propagate context through gRPC requests

--- pkg/router/server.go
+++ pkg/router/server.go
@@ -25,6 +25,10 @@ func (r *routerServer) whereToForward(block string) (shardNodeID int) {
 	return int(math.Mod(float64(h), float64(len(r.shardNodeRPCClients))))
 }
 
+//TODO: another approach for discovering the leader:
+// The cluster sends a leader change message to a queue (this way it doesn't know the clients) or to the clients
+// The clients change their knowledge of the current leader after receiving this message.
+
 // TODO: when the previous leader is down the client should call a random replica node to understand about the new leader
 func (r *routerServer) Read(ctx context.Context, readRequest *pb.ReadRequest) (*pb.ReadReply, error) {
 	whereToForward := r.whereToForward(readRequest.Block)
@@ -32,7 +36,7 @@ func (r *routerServer) Read(ctx context.Context, readRequest *pb.ReadRequest) (*
 
 	//The client sends the request to the current leader
 	currentLeader := r.shardNodeLeaderNodeIDMap[whereToForward]
-	reply, err := shardNodeRPCClient[currentLeader].ClientAPI.Read(context.Background(),
+	reply, err := shardNodeRPCClient[currentLeader].ClientAPI.Read(ctx,
 		&shardnodepb.ReadRequest{Block: readRequest.Block})
 	if err != nil {
 		fmt.Println(err)
@@ -54,7 +58,7 @@ func (r *routerServer) Write(ctx context.Context, writeRequest *pb.WriteRequest)
 
 	//The client sends the request to the current leader
 	currentLeader := r.shardNodeLeaderNodeIDMap[whereToForward]
-	reply, err := shardNodeRPCClient[currentLeader].ClientAPI.Write(context.Background(),
+	reply, err := shardNodeRPCClient[currentLeader].ClientAPI.Write(ctx,
 		&shardnodepb.WriteRequest{Block: writeRequest.Block, Value: writeRequest.Value})
 	if err != nil {
 		return &pb.WriteReply{Success: reply.Success}, err
