7951a309f78e84467bcb7bb8d4a6040502f91da3
aminst <aminst79@gmail.com>
2023-06-11 01:14:00 +0330

    Add oramnode protos

--- scripts/generate_protos.sh
+++ scripts/generate_protos.sh
@@ -6,5 +6,8 @@ protoc --proto_path=$PROTOBUF_PATH --go_out=$PROTOBUF_PATH/router --go_opt=paths
 mkdir -p $PROTOBUF_PATH/shardnode
 protoc --proto_path=$PROTOBUF_PATH --go_out=$PROTOBUF_PATH/shardnode --go_opt=paths=source_relative --go-grpc_out=$PROTOBUF_PATH/shardnode --go-grpc_opt=paths=source_relative shardnode.proto
 
+mkdir -p $PROTOBUF_PATH/oramnode
+protoc --proto_path=$PROTOBUF_PATH --go_out=$PROTOBUF_PATH/oramnode --go_opt=paths=source_relative --go-grpc_out=$PROTOBUF_PATH/oramnode --go-grpc_opt=paths=source_relative oramnode.proto
+
 mkdir -p $PROTOBUF_PATH/leadernotif
 protoc --proto_path=$PROTOBUF_PATH --go_out=$PROTOBUF_PATH/leadernotif --go_opt=paths=source_relative --go-grpc_out=$PROTOBUF_PATH/leadernotif --go-grpc_opt=paths=source_relative leadernotif.proto
