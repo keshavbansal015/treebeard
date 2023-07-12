5fe8e7e554e8cbe18ce9d52269417280e66fc2ef
aminst <aminst79@gmail.com>
2023-07-12 18:22:53 +0330

    Test oramnode raft logic

---
+++ pkg/oramnode/raft_test.go
@@ -0,0 +1,67 @@
+package oramnode
+
+import "testing"
+
+func TestHandleOffsetListReplicationCommandAddsNewOffsetList(t *testing.T) {
+	fsm := newOramNodeFSM()
+	fsm.offsetListMap["request1"] = []int{0, 0, 1, 1}
+	fsm.handleOffsetListReplicationCommand("request2", []int{0, 0, 0, 0})
+	fsm.mu.Lock()
+	defer fsm.mu.Unlock()
+	if _, exists := fsm.offsetListMap["request2"]; !exists {
+		t.Errorf("Expected request2 to have offsetList but not found")
+	}
+	for _, el := range fsm.offsetListMap["request2"] {
+		if el != 0 {
+			t.Errorf("Expected offset list to have 0 offsets but got: %d", el)
+		}
+	}
+}
+
+func TestHandleOffsetListReplicationCommandKeepsOtherOffsetLists(t *testing.T) {
+	fsm := newOramNodeFSM()
+	fsm.offsetListMap["request1"] = []int{0, 0, 1, 1}
+	fsm.handleOffsetListReplicationCommand("request2", []int{0, 0, 0, 0})
+	fsm.mu.Lock()
+	defer fsm.mu.Unlock()
+	if _, exists := fsm.offsetListMap["request1"]; !exists {
+		t.Errorf("Expected request1 to have offsetList but not found")
+	}
+}
+
+func TestHandleDeleteOffsetListReplicationCommandRemovesElementFromOffsetListMap(t *testing.T) {
+	fsm := newOramNodeFSM()
+	fsm.offsetListMap["request1"] = []int{0, 0, 1, 1}
+	fsm.handleDeleteOffsetListReplicationCommand("request1")
+	fsm.mu.Lock()
+	defer fsm.mu.Unlock()
+	if _, exists := fsm.offsetListMap["request1"]; exists {
+		t.Errorf("DeleteOffsetListReplicationCommand should delete offset list")
+	}
+}
+
+func TestHandleBeginEvictionCommandAddsUnfinishedEviction(t *testing.T) {
+	fsm := newOramNodeFSM()
+	fsm.unfinishedEviction = &beginEvictionData{path: 1, storageID: 2}
+	fsm.handleBeginEvictionCommand(32, 54)
+	fsm.mu.Lock()
+	defer fsm.mu.Unlock()
+	if fsm.unfinishedEviction.path != 32 {
+		t.Errorf("Expected an unfinished eviction with path 32 but found path equal to: %d", fsm.unfinishedEviction.path)
+	}
+	if fsm.unfinishedEviction.storageID != 54 {
+		t.Errorf("Expected an unfinished eviction with storageID 54 but found path equal to: %d", fsm.unfinishedEviction.storageID)
+	}
+}
+
+func TestHandleEndEvictionCommandRemovesUnfinishedEviction(t *testing.T) {
+	fsm := newOramNodeFSM()
+	fsm.unfinishedEviction = &beginEvictionData{path: 1, storageID: 2}
+	fsm.handleEndEvictionCommand()
+	fsm.mu.Lock()
+	defer fsm.mu.Unlock()
+
+	if fsm.unfinishedEviction != nil {
+		t.Errorf("handleEndEvictionCommand should empty unfinished evictions")
+	}
+}
