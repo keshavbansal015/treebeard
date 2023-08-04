37361a5718bfc69ad1ccd426f9ee89b500a203e2
aminst <aminst79@gmail.com>
2023-08-04 23:25:26 +0330

    Remove offsetList replication message

--- pkg/oramnode/raft_msg.go
+++ pkg/oramnode/raft_msg.go
@@ -9,9 +9,7 @@ import (
 type CommandType int
 
 const (
-	ReplicateOffsetList CommandType = iota
-	ReplicateDeleteOffsetList
-	ReplicateBeginEviction
+	ReplicateBeginEviction CommandType = iota
 	ReplicateEndEviction
 )
 
@@ -23,42 +21,6 @@ type Command struct {
 
 type ReplicateOffsetListPayload struct {
 	OffsetList []int
-}
-
-func newReplicateOffsetListCommand(requestID string, offsetList []int) ([]byte, error) {
-	payload, err := msgpack.Marshal(
-		&ReplicateOffsetListPayload{
-			OffsetList: offsetList,
-		},
-	)
-	if err != nil {
-		return nil, fmt.Errorf("could not marshal the offsetList replication payload %s", err)
-	}
-	command, err := msgpack.Marshal(
-		&Command{
-			Type:      ReplicateOffsetList,
-			RequestID: requestID,
-			Payload:   payload,
-		},
-	)
-	if err != nil {
-		return nil, fmt.Errorf("could not marshal the offsetList replication command %s", err)
-	}
-	return command, nil
-}
-
-func newReplicateDeleteOffsetListCommand(requestID string) ([]byte, error) {
-	command, err := msgpack.Marshal(
-		&Command{
-			Type:      ReplicateDeleteOffsetList,
-			RequestID: requestID,
-			Payload:   []byte{},
-		},
-	)
-	if err != nil {
-		return nil, fmt.Errorf("could not marshal the delete offsetList command; %s", err)
-	}
-	return command, nil
 }
 
 type ReplicateBeginEvictionPayload struct {
