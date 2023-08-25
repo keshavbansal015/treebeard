4227051dab535cb0c4e91529e07225f463d73337
aminst <aminst79@gmail.com>
2023-08-25 18:59:49 -0400

    Read requests from trace files

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -19,11 +19,27 @@ func main() {
 		log.Fatalf("Failed to start clients; %v", err)
 	}
 
-	testRouterRPCClient := rpcClients[0]
-	value, err := testRouterRPCClient.Read("a")
+	requests, err := client.ReadTraceFile("../../traces/simple.trace")
 	if err != nil {
-		log.Printf("Failed to call Read on router; %v", err)
-		return
+		log.Fatalf("Failed to read trace file; %v", err)
 	}
-	fmt.Printf("Sucess in Read. Got value: %v", value)
+
+	testRouterRPCClient := rpcClients[0] // TODO: randomly choose a router
+	for _, request := range requests {
+		if request.OperationType == client.Read {
+			value, err := testRouterRPCClient.Read(request.Block)
+			if err != nil {
+				log.Printf("Failed to call Read on router; %v\n", err)
+				return
+			}
+			fmt.Printf("Sucess in Read. Got value: %v", value)
+		} else {
+			value, err := testRouterRPCClient.Write(request.Block, request.NewValue)
+			if err != nil {
+				log.Printf("Failed to call Write on router; %v", err)
+				return
+			}
+			fmt.Printf("Sucess in Write. Success: %v\n", value)
+		}
+	}
 }
