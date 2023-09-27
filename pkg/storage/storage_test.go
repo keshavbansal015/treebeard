dea4f8e1c1b4fc7337f547cdae2fbcec52edc692
aminst <aminst79@gmail.com>
2023-09-27 14:52:49 -0400

    Fix TestGetBucketsInPathsReturnsAllBucketIDsInPath

--- pkg/storage/storage_test.go
+++ pkg/storage/storage_test.go
@@ -7,13 +7,17 @@ import (
 func TestGetBucketsInPathsReturnsAllBucketIDsInPath(t *testing.T) {
 	s := NewStorageHandler()
 	buckets, err := s.GetBucketsInPaths([]int{1})
-	expected := []int{4, 2, 1}
+	expectedMap := map[int]bool{
+		4: true,
+		2: true,
+		1: true,
+	}
 	if err != nil {
 		t.Errorf("expected no erros in GetBucketsInPaths")
 	}
-	for i, bucket := range buckets {
-		if bucket != expected[i] {
-			t.Errorf("expected bucketID to be %d, but got %d", expected[i], bucket)
+	for _, bucket := range buckets {
+		if _, exists := expectedMap[bucket]; !exists {
+			t.Errorf("expected bucketID %d to exist", bucket)
 		}
 	}
 }
