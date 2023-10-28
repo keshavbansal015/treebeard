1f1e2f99570c23d3fb1a60b1921e8d6554bfc51b
aminst <aminst79@gmail.com>
2023-10-27 23:19:22 -0400

    Make hasher thread-safe

--- pkg/utils/hash.go
+++ pkg/utils/hash.go
@@ -1,14 +1,25 @@
 package utils
 
-import "hash/fnv"
+import (
+	"hash/fnv"
+	"sync"
+)
 
+// Hahsher is thread-safe.
+// If you see contention on the mutex,
+// you may want to consider a sync.Map.
+// Since we only write to the map once per key,
+// and read from it many times, it might make sense.
 type Hasher struct {
 	KnownHashes map[string]uint32
+	mu          sync.Mutex
 }
 
 // It calculates hash of a string.
 // If the hash is already known it uses KnownHashes.
 func (h *Hasher) Hash(s string) uint32 {
+	h.mu.Lock()
+	defer h.mu.Unlock()
 	if hash, exists := h.KnownHashes[s]; exists {
 		return hash
 	}
