fd3a68fdd0aad09c3030f534fc8fd767c2400d81
aminst <aminst79@gmail.com>
2023-10-31 15:42:29 -0400

    Update logging to print to console when logpath is not provided

--- pkg/utils/logging.go
+++ pkg/utils/logging.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"os"
+	"time"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -14,7 +15,10 @@ func InitLogging(isLogEnabled bool, logPath string) {
 		zerolog.SetGlobalLevel(zerolog.Disabled)
 	}
 
-	logFile, _ := os.OpenFile(logPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-
-	log.Logger = log.Output(logFile)
+	if logPath == "" {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339})
+	} else {
+		logFile, _ := os.OpenFile(logPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+		log.Logger = log.Output(logFile)
+	}
 }
