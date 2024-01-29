080eba9a6fbef1c885a25c4e81a62d1a36db1a80
aminst <aminst79@gmail.com>
2024-01-28 20:10:22 -0500

    Fix block size test

--- pkg/client/trace_test.go
+++ pkg/client/trace_test.go
@@ -8,7 +8,7 @@ func TestPadBlockValueMakesBlocksOfCorrectSize(t *testing.T) {
 	blockSizeBytes := 10
 	blockValue := "1234567"
 	paddedBlockValue := padBlockValue(blockValue, blockSizeBytes)
-	if len(paddedBlockValue) == blockSizeBytes {
+	if len(paddedBlockValue) != blockSizeBytes {
 		t.Errorf("padded block value is not of the correct size")
 	}
 }
