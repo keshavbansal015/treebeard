270ee78547447e80281e3a1f698d53bbb7bc2b0c
aminst <aminst79@gmail.com>
2024-02-27 16:56:11 -0500

    Fix dummy block errors

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -134,11 +134,8 @@ func (s *StorageHandler) BatchGetBlockOffset(bucketIDs []int, storageID int, blo
 					BlockFound: "dummy1",
 				}
 			} else {
-				blockoffsetStatuses[bucketID] = BlockOffsetStatus{
-					Offset:     0,
-					IsReal:     false,
-					BlockFound: "dummy1",
-				}
+				log.Error().Msgf("Did not find valid dummy block in bucket %d", bucketID)
+				return nil, err
 			}
 		}
 	}
