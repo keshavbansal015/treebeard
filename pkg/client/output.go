63b1d8029c2044616b3aa1dfe912640072fe1cc7
aminst <aminst79@gmail.com>
2024-02-26 22:23:17 -0500

    Write average throughput to output file

--- pkg/client/output.go
+++ pkg/client/output.go
@@ -11,9 +11,12 @@ func WriteOutputToFile(outputFilePath string, responseCount []ResponseCount) err
 		return err
 	}
 	defer file.Close()
+	sum := 0.0
 	for _, count := range responseCount {
 		throughput := float64(count.readOperations + count.writeOperations)
+		sum += throughput
 		file.WriteString(fmt.Sprintf("Throughput: %f\n", throughput))
 	}
+	file.WriteString(fmt.Sprintf("Average Throughput: %f\n", sum/float64(len(responseCount))))
 	return nil
 }
